5a5238dd2aebc2954a275ff8bf94f2e371110b1d
Dorian Peters <dpeters08@gmail.com>
2019-11-23 23:14:02 -0800

    Added holiday exceptions for 2020 and 2021

--- app.js
+++ app.js
@@ -7,12 +7,24 @@ while (courtDate.getDay() != 3) {  // increment date when not Wednesday.
     courtDate.setDate(courtDate.getDate() + 1);
 }
 
+// Skip Veterans day in 2020
+if ( (courtDate.getMonth() === 10) && 
+(courtDate.getDate() === 11) && 
+(courtDate.getFullYear() === 2020) ) 
+{
+    courtDate.setDate(courtDate.getDate() + 7);
+}
+
+// Skip Cesar Chavez day in 2021
+if ( (courtDate.getMonth() === 2) && 
+(courtDate.getDate() === 31) && 
+(courtDate.getFullYear() === 2021) ) 
+{
+    courtDate.setDate(courtDate.getDate() + 7);
+}
+
 var iDate = new Date(courtDate);
 var mDate = new Date(courtDate);
-// iDate.setHours(8);
-// iDate.setMinutes(0);
-// mDate.setHours(13);
-// mDate.setMinutes(30);
 
 iDateSpan.textContent = iDate.toDateString() + ' 8:00 AM';
 mDateSpan.textContent = mDate.toDateString() + ' 1:30 PM';
